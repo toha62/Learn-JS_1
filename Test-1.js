0de451d025b4dde51a83e50da964f162b62d2cb0
Anton Trifonov <toha62@inbox.ru>
2022-02-28 17:58:41 +0300

    28.02.2022 17:58

--- Test-1.js
+++ Test-1.js
@@ -1,5 +1,9 @@
 "use strict"
 
-let name_id;
-name_id = prompt("Представтесь пожалуйста", "");
-alert(name_id);
+let name_id = prompt("Какое 'официальное название' JavaScript?", "");
+
+if (name_id == "ECMAScript") {
+  alert("Верно!");
+} else {
+  alert("Не знаете? “ECMAScript”!");
+}
